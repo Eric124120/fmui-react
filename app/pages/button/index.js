98f74328f5342cfc6b97c01d78c75382d78d4ef4
huangchengwen <17710053465@163.com>
2017-04-13 15:23:39 +0800

    button demo add buttonGroup

--- app/pages/button/index.js
+++ app/pages/button/index.js
@@ -3,6 +3,7 @@
  */
 import { Component } from 'react'
 import Button from '../../../packages/button'
+import ButtonGroup from '../../../packages/buttonGroup'
 import './button.scss'
 
 export default class ButtonDom extends Component {
@@ -24,7 +25,7 @@ export default class ButtonDom extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="demo-button-container">
 				<div className="fm-flexbox">
 					<Button size="large" onClick={this.showToast}>default</Button>
 				</div>
@@ -61,6 +62,20 @@ export default class ButtonDom extends Component {
 					<Button type="white-blue">white-blue</Button>
 					<Button type="blue-white">blue-white</Button>
 					<Button disabled>disabled</Button>
+				</div>
+				<div className="btn-group">
+					<h3>button Group justify</h3>
+					<ButtonGroup justify={true}>
+						<Button size='large' type='white-orange'>
+							按钮一
+						</Button>
+						<Button size='large' type='white-orange'>
+							按钮二
+						</Button>
+						<Button size='large' type='white-orange'>
+							按钮三
+						</Button>
+					</ButtonGroup>
 				</div>
 			</div>
 		)
